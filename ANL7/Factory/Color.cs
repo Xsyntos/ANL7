c8d544c81812faea42e09676dca23708ab5feb82
Xsyntos <stevenschool2003@hotmail.com>
2022-06-23 17:34:54 +0200

    ja zieke notes

--- ANL7/Factory/Color.cs
+++ ANL7/Factory/Color.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Steven Rietberg 1008478
+ * Yannick de Vreede 1009289
+ */
 namespace ANL7
 {
     abstract class ColorFactory
